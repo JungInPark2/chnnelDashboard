fa5fa3c024beab81bbe6e31b78be52cb571ce01f
KunYoungLee <cucoco123@gmail.com>
2023-09-22 09:53:32 +0900

    XmpLog 수정

--- src/components/xmpLog/XmpLog.vue
+++ src/components/xmpLog/XmpLog.vue
@@ -19,9 +19,13 @@ const iqryEndDttm = ref('');
 const guid = ref('');
 const ipAddress = ref('');
 const csno = ref('');
+const loading = ref(false);
 
 // API 호출 함수
 const fetchData = async () => {
+
+    loading.value = true;
+		setTimeout(() => (loading.value = false), 1000);
 
     // const serviceName = dropdownValue.value.code; // 시스템 구분
     // const gteDttm = iqrySrtDttm.value;  // 조회 시작일시
@@ -32,14 +36,8 @@ const fetchData = async () => {
 
     const response = await searchXmpLogInfo(dropdownValue.value.code, iqrySrtDttm.value, iqryEndDttm.value, guid.value, ipAddress.value, csno.value);
     apiResponse.value = response;
-    // console.log("apiResponse",apiResponse);
-    // console.log("apiResponse.value.took",apiResponse.value.took);
 
-    // data.value = apiResponse.value.hits.hits;
-    // console.log("data",data);
-    // console.log("data.fields[@timestamp]",data.fields);
-
-    // API 응답에서 필요한 데이터를 추출합니다.
+    // API 응답에서 필요한 데이터 추출
     iqryCn.value = apiResponse.value.hits.hits.map(hit => {
         return {
             dttm: hit.fields["@timestamp"][0], 
@@ -60,42 +58,70 @@ const fetchData = async () => {
                 <Card>
                     <template #title> 조회조건 </template>
                     <template #content>
-                        <div class="card flex flex-column md:flex gap-3">
-                            <div class="p-inputgroup flex-1">
-                                <Dropdown v-model="dropdownValue" :options="dropdownValues" optionLabel="name" placeholder="시스템구분" />
-
-                                <span class="p-inputgroup-addon">
-                                    <i class="pi pi-calendar"></i>
-                                </span>
-                                <Calendar id="calendar-24h" v-model="iqrySrtDttm" showTime hourFormat="24" dateFormat="yy.mm.dd" />
-                                ~
-                                <Calendar id="calendar-24h" v-model="iqryEndDttm" showTime hourFormat="24" dateFormat="yy.mm.dd" />
-
-                                <span class="p-inputgroup-addon">
-                                    <i class="pi pi-file"></i>
-                                </span>
-                                <InputText v-model="guid" placeholder="GUID" />
-
-                                <span class="p-inputgroup-addon">
-                                    <i class="pi pi-wifi"></i>
-                                </span>
-                                <InputText v-model="ipAddress" placeholder="IP주소" />
-
-                                <span class="p-inputgroup-addon">
-                                    <i class="pi pi-user"></i>
-                                </span>
-                                <InputText v-model="csno" placeholder="고객번호" /> 
-
-                                <Button label="조회" class="p-button-help p-button-text mr-2 mb-2" @click="fetchData" />
+                        <form @submit="fetchData" class="flex flex-column gap-2">
+                            <div class="card flex flex-column md:flex gap-3">
+                                <div class="grid p-fluid">
+                                    <div class="col-2 md:col-3">
+                                        <span class="p-float-label">
+                                            <Dropdown id = "dropdownValue" v-model="dropdownValue" :options="dropdownValues" optionLabel="name" placeholder="시스템구분" />
+                                            <label for="dropdownValue">시스템구분</label>
+                                        </span>
+                                    </div>
+                                    <div class="col-12 md:col-3">
+                                        <span class="p-float-label">
+                                            <Calendar :showIcon="true" id="calendar-24h" v-model="iqrySrtDttm" showTime hourFormat="24" dateFormat="yy.mm.dd" />
+                                            <label for="calendar-24h">조회시작일시</label>
+                                        </span>
+                                    </div>
+                                    <div class="col-12 md:col-3">
+                                        <span class="p-float-label">
+                                            <Calendar :showIcon="true" id="calendar-24h" v-model="iqryEndDttm" showTime hourFormat="24" dateFormat="yy.mm.dd" />
+                                            <label for="calendar-24h">조회종료일시</label>
+                                        </span>
+                                    </div>
+                                    <div class="col-12 md:col-3">
+                                        <span class="p-input-icon-left p-input-icon-right p-float-label">
+                                            <i class="pi pi-file" />
+                                            <InputText id="guid" v-model="guid" type="text" />
+                                            <label for="guid">GUID</label>
+                                            <i class="pi pi-search" />
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="grid p-fluid">
+                                    <div class="col-12 md:col-3">
+                                        <span class="p-input-icon-left p-input-icon-right p-float-label">
+                                            <i class="pi pi-wifi" />
+                                            <InputText id="ipAddress" v-model="ipAddress" type="text" />
+                                            <label for="guid">IP주소</label>
+                                            <i class="pi pi-search" />
+                                        </span>
+                                    </div>
+                                    <div class="col-12 md:col-3">
+                                        <span class="p-input-icon-left p-input-icon-right p-float-label">
+                                            <i class="pi pi-user" />
+                                            <InputText id="csno" v-model="csno" type="text" />
+                                            <label for="guid">고객번호</label>
+                                            <i class="pi pi-search" />
+                                        </span>
+                                    </div>
+                                    <div class="col-12 md:col-3">
+                                        <Button type="button"  icon="pi pi-search" class="mr-2 mb-2" :loading="loading" @click="fetchData()" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+
+                        </form>
+                        
                     </template>
                 </Card>
                 <Card>
                     <template #title> 조회결과 </template>
                     <template #content>
                         <div class="card">
-                            <DataTable :value="iqryCn" sortMode="multiple" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem">
+                            <DataTable :value="iqryCn" sortMode="multiple" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem" :loading="loading">
+                                <template #empty> 결과가 없습니다. </template>
+					            <template #loading> </template>
                                 <Column field="dttm" header="일시" sortable style="width: 20%"></Column>
                                 <Column field="guid" header="GUID" sortable style="width: 20%"></Column>
                                 <Column field="ipAddress" header="IP주소" sortable style="width: 20%"></Column>
