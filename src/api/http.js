eee74371b3f027ff7dd915bc8304cddf39daa28e
KunYoungLee <cucoco123@gmail.com>
2023-10-23 17:22:26 +0900

    http url 수정 및 ApiKey 요청 변경

--- src/api/http.js
+++ src/api/http.js
@@ -5,17 +5,16 @@ import setupMock from "@/api/mock/setupMock";
 const http = axios.create({
     headers: {
         'Content-type': 'application/json',
-        'Authorization': 'ApiKey ${import.meta.env.VITE_APP_API_KEY}'
+        'Authorization': 'ApiKey import.meta.env.VITE_APP_API_KEY'
     },
     baseURL: import.meta.env.VITE_APP_API_URL,
     timeout:10000    // 타임아웃 설정 (10초)
 });
 
 // Local인 경우 mock 데이터 응답
-// if(import.meta.env.MODE === 'L'){
-//      console.log('%c This is Mock Mode!','color:red');
-//      setupMock(new MockAdapter(http));
-
-//  }
+ if(import.meta.env.MODE === 'L'){
+      console.log('%c This is Mock Mode!','color:red');
+      setupMock(new MockAdapter(http));
+ }
 
 export default http;
