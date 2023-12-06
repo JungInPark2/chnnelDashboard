12a9c102f1a53e6b41188412111ffbd344272393
KunYoungLee <cucoco123@gmail.com>
2023-12-06 10:19:06 +0900

    Local인 경우 mock 데이터 응답 주석 해제

--- src/api/http.js
+++ src/api/http.js
@@ -21,9 +21,9 @@ http.interceptors.request.use(config => {
 });
 
 // Local인 경우 mock 데이터 응답
-//  if(import.meta.env.MODE === 'L'){
-//       console.log('%c This is Mock Mode!','color:red');
-//       setupMock(new MockAdapter(http));
-//  }
+  if(import.meta.env.MODE === 'L'){
+       console.log('%c This is Mock Mode!','color:red');
+       setupMock(new MockAdapter(http));
+  }
 
 export default http;
